0707a2ea57342f9d7b83a9dc13a282fb3d7314ea
KustelR <dmitriysstudios2@gmail.com>
2025-05-15 20:08:54 +0400

    Add layout and page components for emulator interface

---
+++ src/app/em6502/layout.tsx
@@ -0,0 +1,43 @@
+import AppHeader from "@/components/AppHeader";
+import Link from "next/link";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-screen w-screen flex flex-col font-[family-name:var(--font-geist-mono)]">
+      <AppHeader />
+      <div className="flex h-full flex-row p-3 space-x-4">
+        <EmulatorNav />
+        {children}
+      </div>
+    </div>
+  );
+}
+function EmulatorNav() {
+  return (
+    <div className="space-y-2 bg-neutral-800 h-full">
+      <header className="px-2 bg-white/10">Emulator</header>
+      <ul className="[&_li]:px-2 [&_li]:hover:bg-white/5 [&_li]:cursor-pointer">
+        <li>
+          <Link className="w-full block" href="/em6502">
+            Editor
+          </Link>
+        </li>
+        <li>
+          <Link className="w-full block" href="/em6502/memory">
+            Inspect Memory
+          </Link>
+        </li>
+        <li>
+          <Link className="w-full block" href="/em6502/hardware">
+            Inspect Hardware
+          </Link>
+        </li>
+        <li>
+          <Link className="w-full block" href="/em6502/faq">
+            Help IDK
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}

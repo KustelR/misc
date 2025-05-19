53f76c7a42999d667d51cf2f118f48f16c694004
KustelR <dmitriysstudios2@gmail.com>
2025-05-19 12:46:47 +0400

    Integrate CPU state management into page component and update CPUStateView to reflect CPU state

--- src/app/em6502/page.tsx
+++ src/app/em6502/page.tsx
@@ -1,11 +1,42 @@
+"use client";
+
 import CPUStateView from "@/components/CPUStateView";
 import Editor from "@/components/Editor";
+import { CPU, ByteRegister } from "@/emulator/cpu";
+import { AddressingMode, CommandType } from "@/emulator/instructions";
+import { DoubleWord, Word } from "@/emulator/memory";
+import { useEffect, useState } from "react";
 
 export default function () {
+  const [cpu, setCpu] = useState(new CPU());
+  useEffect(() => {
+    cpu.writeMemory(new DoubleWord(17), new Word(2));
+    cpu.perform({
+      command: {
+        commandType: CommandType.adc,
+        addressingMode: AddressingMode.immediate,
+      },
+      trailingBytes: [new Word(129)],
+    });
+    cpu.perform({
+      command: {
+        commandType: CommandType.adc,
+        addressingMode: AddressingMode.immediate,
+      },
+      trailingBytes: [new Word(128)],
+    });
+    cpu.perform({
+      command: {
+        commandType: CommandType.sda,
+        addressingMode: AddressingMode.zeroPage,
+      },
+      trailingBytes: [new Word(1)],
+    });
+  }, [cpu]);
   return (
     <main className="flex flex-row space-x-6 h-full space-y-4">
       <CodeView />
-      <CPUStateView />
+      <CPUStateView cpu={cpu} />
     </main>
   );
 }

4cc014469123eaf9fb830d94704c0df1e18b5440
KustelR <dmitriysstudios2@gmail.com>
2025-05-27 13:21:31 +0400

    feat: actual compilation

--- src/emulator/compiler.ts
+++ src/emulator/compiler.ts
@@ -8,7 +8,7 @@ import { Word } from "./memory";
 
 const tokenRegex = /([;:a-zA-Z0-9$(#)_]+(?:,\s?[XY])?)/gm;
 const labelDeclarationRegex = /^([a-zA-Z_][a-zA-Z0-9_]*):$/;
-const numberRegex = /\$?([0-9a-f]+)/gm;
+const byteRegex = /[0-9a-f]{2}/gm;
 
 const accumulatorAddressRegex = /^A$/;
 const zeroPageAddressRegex = /\$[0-9a-f]{2}$/gm;
@@ -17,47 +17,65 @@ const zeroPageXAddressRegex = /\$[0-9a-f]{2},\s?[X]/gm;
 const zeroPageYAddressRegex = /\$[0-9a-f]{2},\s?[Y]/gm;
 
 export function compile(source: string): Array<Word> {
+  const precompiled = precompile(source);
+  return assemble(precompiled);
+}
+
+function precompile(source: string): string {
+  // TODO: Replace labels here
+  return source;
+}
+
+function assemble(source: string): Array<Word> {
   const result: Word[] = [];
   const lines = source.split("\n");
   lines.forEach((line) => {
     const trimmed = line.trim();
-    if (!trimmed || trimmed.startsWith(";")) return; // Skip empty lines and comments
-    const tokens = line.match(tokenRegex);
-    if (tokens) {
-      let instruction: CommandType | undefined;
-      let trailingBytes: Word[] = [];
-      let addressingMode: AddressingMode | undefined;
-      if (labelDeclarationRegex.test(tokens[0])) {
-        // Handle label definition
-        throw new Error("Labels not implemented yet");
-      }
-      instruction = CommandType[tokens[0] as keyof typeof CommandType];
-      if (!instruction) throw new Error(`Unknown instruction: ${tokens[0]}`);
-      if (tokens.length === 1) {
-        const addressingModes = getAddressingModes(instruction);
-        const addrM = Number(Object.entries(addressingModes[0]));
-        addressingMode = addrM;
-      }
-      if (addressingMode === undefined)
-        throw new Error(
-          `Unknown addressing mode: ${addressingMode ? AddressingMode[addressingMode] : "undefined"}`,
-        );
-      const command = cmdToByte({
-        commandType: instruction,
-        addressingMode: addressingMode,
-      });
-      if (command) {
-        result.push(command);
-      }
-    }
+    if (!trimmed || trimmed.startsWith(";")) return;
+
+    const compiled = assembleLine(trimmed);
+    result.push(...compiled);
   });
+  return result;
+}
+
+function assembleLine(line: string) {
+  const result: Word[] = [];
+  const tokens = line.match(tokenRegex);
+  if (tokens) {
+    let instruction: CommandType | undefined;
+    let addressingMode: AddressingMode | undefined;
+    if (labelDeclarationRegex.test(tokens[0])) {
+      throw new Error("Encountered label in assemble step");
+    }
+    instruction = CommandType[tokens[0] as keyof typeof CommandType];
+
+    if (!instruction) throw new Error(`Unknown instruction: ${tokens[0]}`);
+    addressingMode = getAddressMode(instruction, tokens.length, tokens[1]);
+
+    const command = cmdToByte({
+      commandType: instruction,
+      addressingMode: addressingMode,
+    });
+    let trailingBytes: Word[] = [];
+    if (tokens.length > 1) trailingBytes = getArgumentBytes(tokens[1]);
+    if (command) {
+      result.push(command);
+      result.push(...trailingBytes);
+    }
+  }
   return result;
 }
 
 function getAddressMode(
   commandType: CommandType,
+  tokensLength: number,
   addressToken: string,
-): AddressingMode | undefined {
+): AddressingMode {
+  if (tokensLength === 1) {
+    const addressingModes = getAddressingModes(commandType);
+    return Number(Object.entries(addressingModes[0]));
+  }
   if (accumulatorAddressRegex.test(addressToken)) {
     return AddressingMode.accumulator;
   }
@@ -79,13 +97,11 @@ function getAddressMode(
   if (addressToken.startsWith("*") || commandType === CommandType.jmp) {
     return AddressingMode.relative;
   }
-  return;
+  throw new Error("Can't determine addressing mode");
 }
 
-function getNumber(token: string): number | undefined {
-  const match = token.match(numberRegex);
-  if (match) {
-    return parseInt(match[1], 16);
-  }
-  return;
+function getArgumentBytes(token: string): Word[] {
+  const match = token.match(byteRegex);
+  if (!match) throw new Error(`Invalid argument: ${token}`);
+  return match.map((byte) => new Word(parseInt(byte, 16))).reverse();
 }
